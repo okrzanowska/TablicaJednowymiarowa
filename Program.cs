54a07230b026d45047d3b1c80c10a495b3cb0313
Oliwia <106619574+okrzanowska@users.noreply.github.com>
2024-02-10 13:21:20 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TablicaJednowymiarowe
+{
+    class Program
+    {
+        private static void Main()
+        {
+            var table = new int[10];
+
+            for (var i = 0; i < table.Length; i++)
+            {
+                table[i] = i;
+            }
+
+            Console.WriteLine("Index\tValue");
+            for (var i = 0; i < table.Length; i++)
+            {
+                Console.WriteLine($"{i}\t{table[i]}");
+            }
+        }
+    }
+}
